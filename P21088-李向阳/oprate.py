0571aa0572ee9aefe3109d21bdd497a93de4183c
pythonlixiangyang <48939064+pythonlixiangyang@users.noreply.github.com>
2019-04-15 14:20:52 +0800

    Create oprate.py

---
+++ P21088-李向阳/oprate.py
@@ -0,0 +1,15 @@
+n = 0
+while n == 0:
+    a = int(input('Input a num:'))
+    b = input('Input a operate: + - * /')
+    c = int(input('Input a num:'))
+    if b ==' +':
+        print(a+c)
+    if b == '-':
+        print(a-c)
+    if b == '*':
+        print(a*c)
+    if b == '/':
+        print(a/c)
+    else:
+        break
