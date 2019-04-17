e857be76a462bd3dc876e8dd0de094c278d89c4c
haozheyu <1216108213@qq.com>
2019-04-17 13:44:54 +0800

    Update 第三周作业.py

--- P21067-郝哲宇/第三周/第三周作业.py
+++ P21067-郝哲宇/第三周/第三周作业.py
@@ -86,6 +86,59 @@ print(id(lst),id(lst1),id(lst2))
 
 这里使用升序和降序
 lst=[3,5,1,7,9,6,8]
+#降序：
+lst=[3,5,1,7,9,6,8]
+length = len(lst)
+print(length)
 
+count = 0
+for i in lst:
+    count+=1
+length1=count
+print(length1)
+
+lst1=[]
+for _ in range(length1):
+    c = 0
+    for i in lst:
+        if i > c:
+            c = i
+    lst.remove(c)
+    lst1.append(c)
+print(lst1)
+
+#升序：
+print(set(lst))
 """
 
+#有如下一个字符串变量logs，请统计出每种请求类型的数量（提示：空格分割的第2列是请求类型），得到如下输出：
+"""
+
+POST 2
+GET 3
+下边是logs变量：
+logs = '''
+111.30.144.7 "POST /mock/login/?t=GET HTTP/1.1" 200 
+111.30.144.7 "Get /mock/users/?t=POST HTTP/1.1" 200 
+111.13.100.92 "Post /mock/login/ HTTP/1.1" 200 
+223.88.60.88 "GET /mock/users/?t=POST HTTP/1.1" 200 
+111.30.144.7 "GET /mock/users/ HTTP/1.1" 200 
+''' 
+
+"""
+logs = '''
+111.30.144.7 "POST /mock/login/?t=GET HTTP/1.1" 200 
+111.30.144.7 "Get /mock/users/?t=POST HTTP/1.1" 200 
+111.13.100.92 "Post /mock/login/ HTTP/1.1" 200 
+223.88.60.88 "GET /mock/users/?t=POST HTTP/1.1" 200 
+111.30.144.7 "GET /mock/users/ HTTP/1.1" 200 
+'''
+
+log_lst = logs.split(" ")
+print(log_lst)
+
+lst1 = log_lst[1:-1:5]
+lst1_keywords=" ".join(lst1)
+lst1_upper=lst1_keywords.upper()
+print('POST',lst1_upper.count('POST'))
+print('GET',lst1_upper.count('GET'))
