899328f51bf0e94e345758da971bb2c99e16b917
Huaxia Sui <48899297+suihuaxia@users.noreply.github.com>
2019-04-22 11:35:03 +0800

    Update (week3)sort.py

--- P21086-睢花夏/(week3)sort.py
+++ P21086-睢花夏/(week3)sort.py
@@ -34,3 +34,13 @@ num = [3,5,1,7,9,6,8]
 num.sort(reverse=True)
 print(num)
 
+5.
+#封装与解构
+num = [3,5,1,7,9,6,8]
+length = len(num)
+
+for i in range(length):
+    for j in range(length-i-1):
+        if num[j] < num[j+1]:
+            num[j],num[j+1] = num[j+1],num[j]
+print(num)
