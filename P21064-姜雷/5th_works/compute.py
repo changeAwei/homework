8130eeeb67c5f66d548e817a13c7f5670bab067f
jianglei7713 <jl7713@163.com>
2019-05-05 15:52:03 +0800

    Update compute.py

--- P21064-姜雷/5th_works/compute.py
+++ P21064-姜雷/5th_works/compute.py
@@ -10,7 +10,7 @@ def compute():
         first = input('please input first number:')
         if first == 'quit':
             break
-        firstnumber = 0 if int(first) == 0 else int(first.strip().lstrip('0'))
+        first = 0 if int(first) == 0 else int(first.strip().lstrip('0'))
 
         operator = input(r'please inpute operator ,which is one of "+", "-", "*", "/":')
         if operator not in opratorlist:
@@ -20,16 +20,16 @@ def compute():
         second = input('please input second number:')
         if second == 'quit':
             break
-        secondnumber = 0 if int(second) == 0 else int(second.strip().lstrip('0'))
+        second = 0 if int(second) == 0 else int(second.strip().lstrip('0'))
 
         if operator == '+':
-            sesult = firstnumber + secondnumber
+            sesult = first + second
         elif operator == '-':
-            sesult = firstnumber - secondnumber
+            sesult = first - second
         elif operator == '*':
-            sesult = firstnumber * secondnumber
+            sesult = first * second
         else:
-            sesult = firstnumber / secondnumber
+            sesult = first / second
         print('the sesult is {}'.format(sesult))
 
 
