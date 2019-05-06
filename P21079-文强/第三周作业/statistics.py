1240c1a4b2ec79ad692a87948b29cca6c666aecd
wuyinhan <898600285@qq.com>
2019-05-06 16:45:05 +0800

    第三周作业

---
+++ P21079-文强/第三周作业/statistics.py
@@ -0,0 +1,13 @@
+#有如下一个字符串变量logs，请统计出每种请求类型的数量（提示：空格分割的第2列是请求类型），得到如下输出：
+#POST 2
+#GET 3
+#下边是logs变量：
+"""
+logs = '''
+111.30.144.7 "POST /mock/login/?t=GET HTTP/1.1" 200
+111.30.144.7 "Get /mock/users/?t=POST HTTP/1.1" 200
+111.13.100.92 "Post /mock/login/ HTTP/1.1" 200
+223.88.60.88 "GET /mock/users/?t=POST HTTP/1.1" 200
+111.30.144.7 "GET /mock/users/ HTTP/1.1" 200
+'''
+"""
